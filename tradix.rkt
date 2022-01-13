64b425090f07e19698acded234b1aefcacb002db
Ethan Wu <ethanwu@cs.ucsb.edu>
2022-01-13 03:20:51 -0800

    Add shortcut flags

--- tradix.rkt
+++ tradix.rkt
@@ -53,15 +53,27 @@
 (module+ main
   (command-line
    #:program (short-program+command-name)
-   #:once-each ;
+   #:once-any ;
    [("-i" "--input-radix")
     input_radix ;
     "The input base. [default: 10]"
     (input-radix (string->number input_radix))]
+   [("-B" "--Binary") "Binary (base 2)." (input-radix 2)]
+   [("-S" "--Senary") "Senary (base 6)." (input-radix 6)]
+   [("--Octal") "Octal (base 8)." (input-radix 8)]
+   [("-D" "--Dozenal") "Dozenal (base 12)." (input-radix 12)]
+   [("-X" "--Hexadecimal") "Hexadecimal (base 16)." (input-radix 16)]
+   #:once-any ;
    [("-o" "--output-radix")
     output_radix ;
     "The output base. [default: 10]"
     (output-radix (string->number output_radix))]
+   [("-b" "--binary") "Binary (base 2)." (output-radix 2)]
+   [("-s" "--senary") "Senary (base 6)." (output-radix 6)]
+   [("--octal") "Octal (base 8)." (output-radix 8)]
+   [("-d" "--dozenal") "Dozenal (base 12)." (output-radix 12)]
+   [("-x" "--hexadecimal") "Hexadecimal (base 16)." (output-radix 16)]
+   [("--sexagesimal") "Sexagesimal (base 60)." (output-radix 60)]
    #:once-any ;
    [("-l" "--list") "Output as a list of digit values." (output-format "list")]
    #:once-any ;
@@ -69,8 +81,9 @@
     alphabet ;
     "Specify the output alphabet."
     (output-alphabet alphabet)]
-   [("--pitman" "--dozenal-pitman")
-    "Isaac Pitman's dozenal numerals."
+   [("--pitman")
+    "Isaac Pitman's dozenal numerals. Implies --dozenal."
+    (output-radix 12)
     (output-alphabet "dozenal-pitman")]
    #:args ([number #f])
    (let* ([number (string->number (string-trim (if number number (port->string (current-input-port))))
